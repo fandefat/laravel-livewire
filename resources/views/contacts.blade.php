7a03af97cf5f2eedb2ab012cabec1b3c08c78c16
fandefat <72208510+fandefat@users.noreply.github.com>
2020-10-01 22:46:40 +0700

    fix size container

--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-md-6">
+            <div class="col-md-8">
                 @livewire('contact-component')
             </div>
         </div>
